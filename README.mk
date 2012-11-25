2f947c46da31f9eeb6ba2be153f0ac0558166911
Drahflow <drahflow@gmx.de>
2012-11-25 23:16:30 +0100

    Readme

---
+++ README.mk
@@ -0,0 +1,12 @@
+Problem
+=======
+
+* You need a static file served as fast as the network can manage.
+* Your box sucks, for example because it's the cheap virtual host you could find.
+
+Solution
+========
+
+The Single-File (HTTP) server.
+* It serves a single file.
+* Fast.
